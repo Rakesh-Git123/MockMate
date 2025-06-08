3bb6aaa4e241b77cd9dd5ae223605550ece625be
Rakesh-Git123 <rakeshgit1234@gmail.com>
2025-06-08 19:46:45 +0530

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ connectDB();
 const app=express();
 const PORT=process.env.PORT || 5000
 
-app.use(cors({ origin: ["http://localhost:5173,https://mockmatepro.netlify.app"], credentials: true }));
+app.use(cors({
+    origin: ["http://localhost:5173", "https://mockmatepro.netlify.app"],
+    credentials: true
+  }));
+  
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
